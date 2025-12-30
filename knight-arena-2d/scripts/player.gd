c2bf72a2e18790d660bbe039715d2426496b047a
DomagalaKamil <139066315+DomagalaKamil@users.noreply.github.com>
2025-12-31 00:23:38 +0100

    basic set up
    - imported player characters
    - imported map art
    - set up basic player animations with skin
    - set up basic player movement

---
+++ knight-arena-2d/scripts/player.gd
@@ -0,0 +1,68 @@
+extends CharacterBody2D
+
+const speed = 100
+var current_direction = "none"
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+	
+func player_movement(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		current_direction = "right"
+		play_animation(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		current_direction = "left"
+		play_animation(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		current_direction = "down"
+		play_animation(1)
+		velocity.x = 0
+		velocity.y = speed
+	elif Input.is_action_pressed("ui_up"):
+		current_direction = "up"
+		play_animation(1)
+		velocity.x = 0
+		velocity.y = -speed
+	else:
+		play_animation(0)
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
+	
+func play_animation(movement):
+	var directon = current_direction
+	var animation = $AnimatedSprite2D
+	
+	if directon == "right":
+		animation.flip_h = false
+		if movement == 1:
+			animation.play("side_walk")
+		elif movement == 0:
+			animation.play("side_idle")
+	if directon == "left":
+		animation.flip_h = true
+		if movement == 1:
+			animation.play("side_walk")
+		elif movement == 0:
+			animation.play("side_idle")
+	if directon == "down":
+		animation.flip_h = false
+		if movement == 1:
+			animation.play("front_walk")
+		elif movement == 0:
+			animation.play("front_idle")
+	if directon == "up":
+		animation.flip_h = false
+		if movement == 1:
+			animation.play("back_walk")
+		elif movement == 0:
+			animation.play("back_idle")
